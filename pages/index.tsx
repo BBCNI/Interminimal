d2f76984013bf3450ac27c4e991ef3fb480f2e34
Andy Armstrong <andy@hexten.net>
2022-05-05 21:02:22 +0100

    Welsh cats again

--- pages/index.tsx
+++ pages/index.tsx
@@ -105,7 +105,7 @@ const dictionary: TDictionaryRoot = {
     // A nested dictionary for use with dictionaryFromTag
     "madness": { $$dict: { site: { en: "Or maybe something else" } } },
     "h.siteName": { en: "It's Called", fr: "C'est appelé" },
-    "h.someCats": { en: "Some Cats", fr: "Quelques Chats" }
+    "h.someCats": { en: "Some Cats", fr: "Quelques Chats", cy: "Rhai Cathod" }
   }
 };
 
