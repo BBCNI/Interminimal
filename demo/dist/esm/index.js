3a16cf481cc43d9b641e587fe968a53be6625730
Andy Armstrong <andy@hexten.net>
2022-05-16 13:01:00 +0100

    More exports, more doc.

--- demo/dist/esm/index.js
+++ demo/dist/esm/index.js
@@ -2,3 +2,6 @@ export * from "./types";
 export * from "./components";
 export * from "./string";
 export * from "./context";
+export * from "./bcp47";
+export * from "./localeStack";
+export * from "./template";
